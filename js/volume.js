41897e0ca4dcbe55f20cf74b9ca81a59cb1e80d6
AriiSharma <arsh1119@colorado.edu>
2023-05-31 14:21:43 -0600

    ICA10 completed.

--- js/volume.js
+++ js/volume.js
@@ -1,34 +1,63 @@
-function increaseVolume() {
-    showLoadingOverlay();
-  
-    setTimeout(function() {
-      hideLoadingOverlay();
-      alert('Volume increased!');
-    }, 3000);
+let volume = 50;
+let birdPosition = 50;
+const volumeLevel = document.querySelector('.volume-level');
+const bird = document.querySelector('#bird');
+const message = document.querySelector('#message');
+
+document.addEventListener('mousemove', handleMouseMove);
+
+function handleMouseMove(event) {
+  const gameArea = document.querySelector('.game-area');
+  const gameAreaHeight = gameArea.clientHeight;
+  const gameAreaTop = gameArea.offsetTop;
+  const mouseY = event.clientY - gameAreaTop;
+
+  if (mouseY > birdPosition && mouseY < gameAreaHeight - birdPosition) {
+    bird.style.top = mouseY + 'px';
   }
-  
-  function decreaseVolume() {
-    showLoadingOverlay();
-  
-    setTimeout(function() {
-      hideLoadingOverlay();
-      alert('Volume decreased!');
-    }, 3000);
+}
+
+function changeVolume(action) {
+  if (action === 'increase') {
+    if (volume < 100) {
+      volume += 10;
+      displayMessage(`Volume increased to ${volume}%!`, 'success');
+    } else {
+      displayMessage("Oops, I can't increase the volume anymore!", 'error');
+    }
+  } else if (action === 'decrease') {
+    if (volume > 0) {
+      volume -= 10;
+      displayMessage(`Volume decreased to ${volume}%!`, 'success');
+    } else {
+      displayMessage("Oops, I can't decrease the volume anymore!", 'error');
+    }
   }
-  
-  function showLoadingOverlay() {
-    var overlay = document.createElement('div');
-    overlay.className = 'overlay';
-  
-    var loadingIcon = document.createElement('div');
-    loadingIcon.className = 'loading-icon';
-  
-    overlay.appendChild(loadingIcon);
-    document.body.appendChild(overlay);
+
+  volumeLevel.textContent = `Volume Level: ${volume}%`;
+
+  if (volume === 0 || volume === 100) {
+    endGame();
   }
-  
-  function hideLoadingOverlay() {
-    var overlay = document.querySelector('.overlay');
-    document.body.removeChild(overlay);
-  }
+}
+
+function displayMessage(msg, type) {
+  message.textContent = msg;
+  message.classList.add(type);
+  message.style.visibility = 'visible';
+
+  setTimeout(() => {
+    message.textContent = '';
+    message.classList.remove(type);
+    message.style.visibility = 'hidden';
+  }, 2000);
+}
+
+function endGame() {
+  displayMessage('Game Over!', 'error');
+  document.removeEventListener('mousemove', handleMouseMove);
+}
+
+
+
   
