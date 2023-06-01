f3d56162998842325d736b0b77e76f547f12840e
AriiSharma <arsh1119@colorado.edu>
2023-06-01 14:43:27 -0600

    Update

--- ica/ica9/ica9.js
+++ ica/ica9/ica9.js
@@ -1,46 +1,50 @@
-const questionButton = document.querySelector('#js-new-quote');
-questionButton.addEventListener('click', newTrivia);
-
-const answerButton = document.querySelector('#js-tweet');
-answerButton.addEventListener('click', giveAnswer);
-
-const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
-
-let currentTrivia = null;
-
-async function newTrivia() {
-  const answerArea = document.querySelector("#js-answer-text");
-  answerArea.textContent = "";
-
-  try {
-    const response = await fetch(endpoint);
-    if (!response.ok) {
-      throw new Error(response.statusText);
+document.addEventListener("DOMContentLoaded", function () {
+    const questionButton = document.querySelector("#js-new-quote");
+    questionButton.addEventListener("click", newTrivia);
+  
+    const answerButton = document.querySelector("#js-tweet");
+    answerButton.addEventListener("click", giveAnswer);
+  
+    const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+  
+    let currentTrivia = null;
+  
+    async function newTrivia() {
+      const answerArea = document.querySelector("#js-answer-text");
+      answerArea.textContent = "";
+  
+      try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+  
+        const jsonData = await response.json();
+  
+        currentTrivia = jsonData;
+  
+        const quoteText = currentTrivia.question;
+        const quoteArea = document.querySelector("#js-quote-text");
+        quoteArea.textContent = quoteText;
+      } catch (err) {
+        console.log(err);
+        alert("Failed to fetch trivia question!");
+      }
     }
-
-    const jsonData = await response.json();
-
-    currentTrivia = jsonData;
-
-    const quoteText = currentTrivia.question;
-    const quoteArea = document.querySelector("#js-quote-text");
-    quoteArea.textContent = quoteText;
-  } catch (err) {
-    console.log(err);
-    alert("Failed to fetch trivia question!");
-  }
-}
-
-function giveAnswer() {
-  if (currentTrivia) {
-    const answerText = currentTrivia.answer;
-    const answerArea = document.querySelector("#js-answer-text");
-    answerArea.textContent = answerText;
-  } else {
-    alert("No trivia question available. Click 'Generate a new bit of trivia!' to get a question.");
-  }
-}
-
+  
+    function giveAnswer() {
+      if (currentTrivia) {
+        const answerText = currentTrivia.answer;
+        const answerArea = document.querySelector("#js-answer-text");
+        answerArea.textContent = answerText;
+      } else {
+        alert(
+          "No trivia question available. Click 'Generate a new bit of trivia!' to get a question."
+        );
+      }
+    }
+  });
+  
 
 
 
